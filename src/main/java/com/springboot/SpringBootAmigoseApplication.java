46b223eccac1c1b4d473b8a7618f8c04e6b4f929
Hamza <ali.hamza@alsharqi.co>
2023-09-25 16:58:56 +0500

    first commit

---
+++ src/main/java/com/springboot/SpringBootAmigoseApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAmigoseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAmigoseApplication.class, args);
+	}
+
+}
