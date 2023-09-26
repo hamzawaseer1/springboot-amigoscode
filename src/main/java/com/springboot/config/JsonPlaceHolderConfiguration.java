5c224a1a699c592006258c9410e25c07aee00709
Hamza <ali.hamza@alsharqi.co>
2023-09-26 17:33:48 +0500

    profile management

--- src/main/java/com/springboot/config/JsonPlaceHolderConfiguration.java
+++ src/main/java/com/springboot/config/JsonPlaceHolderConfiguration.java
@@ -1,14 +1,32 @@
 package com.springboot.config;
 
+import com.springboot.appinfo.ApplicationInformation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import java.util.Arrays;
 
 @Configuration
 public class JsonPlaceHolderConfiguration {
-//    @Bean
-//    public CommandLineRunner jsonPlaceholder(JsonPlaceHolderClient client) {
-//        return args -> {
-//            System.out.println(client.getPosts().size());
-//            System.out.println(client.getPost(1));
-//        };
-//    }
+
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public CommandLineRunner jsonPlaceholder(ApplicationInformation applicationInformation) {
+        return args -> {
+            System.out.println("Command Line runner hurray");
+            System.out.println(companyName);
+            System.out.println(environment.getProperty("info.app.version"));
+            System.out.println(applicationInformation);
+            System.out.println(Arrays.toString(environment.getActiveProfiles()));
+        };
+    }
 }
